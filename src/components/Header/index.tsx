39e10f291319e4d8cefaba7cf93575bdd9f6224f
Djalves424 <djalves424@gmail.com>
2024-06-19 16:52:34 -0300

    Basic route structure

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+         <h1>MeuSite</h1>
+      </div>
+    </header>
+  );
+}
---
+++ src/routes/Home/Promotion/index.tsx
@@ -0,0 +1,8 @@
+export default function Protion() {
+
+     return (
+        <main>
+            <h2>Página de promoção</h2>
+        </main>
+     )
+}
