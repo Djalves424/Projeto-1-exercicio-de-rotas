7915fbfa738819065adafdef3d8fa293fd005cfd
Djalves424 <djalves424@gmail.com>
2024-06-20 19:30:21 -0300

    Promotion component layout created

--- src/routes/Home/Promotion/index.tsx
+++ src/routes/Home/Promotion/index.tsx
@@ -1,8 +1,19 @@
-export default function Protion() {
+import Card from "../../../components/Header/Card";
+import Button from "../../../components/Header/Button";
 
-     return (
-        <main>
-            <h2>Página de promoção</h2>
-        </main>
-     )
+export default function Promotion() {
+  return (
+    <main>
+      <section>
+        <div className="container">
+          <div className="mt30 mb30">
+            <Card title="Página de promoção" />
+          </div>
+          <div className="dflex">
+            <Button text="Quero participar" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
