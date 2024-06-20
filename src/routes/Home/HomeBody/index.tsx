695a6a7a746700b9900315fe798edfc37be01f65
Djalves424 <djalves424@gmail.com>
2024-06-20 19:26:05 -0300

    Styling the Button component

--- src/routes/Home/HomeBody/index.tsx
+++ src/routes/Home/HomeBody/index.tsx
@@ -1,11 +1,17 @@
 import Card from "../../../components/Header/Card";
+import Button from "../../../components/Header/Button";
 
 export default function HomeBody() {
   return (
     <main>
       <section>
-        <div className="container mt30 mb30">
-          <Card title="Página inicial" />
+        <div className="container">
+          <div className="mt30 mb30">
+            <Card title="Página inicial" />
+          </div>
+          <div className="dflex">
+            <Button text="Ver promoção" />
+          </div>
         </div>
       </section>
     </main>
